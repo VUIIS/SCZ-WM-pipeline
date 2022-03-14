f1b750a744e833f8decf9c8ed8d0f7520ed1544a
lawlessrd <r.dylan.lawless@vumc.org>
2022-03-14 12:38:38 -0500

    Fixed json import.

--- matlab/test_compiled_matlab.sh
+++ matlab/test_compiled_matlab.sh
@@ -7,9 +7,9 @@
 # Single fmri input
 bin/run_spm12.sh /usr/local/MATLAB/MATLAB_Runtime/v97 \
     function scz_entrypoint \
-    t1_niigz "/home/dylan/Documents/SCZ/INPUTS/T1.nii.gz" \
-    fmri_niigz "/home/dylan/Documents/SCZ/INPUTS/fmri.nii.gz" \
+    t1_niigz "/home/dylan/Documents/SCZ/INPUTS2/T1.nii.gz" \
+    fmri_niigz "/home/dylan/Documents/SCZ/INPUTS2/fmri.nii.gz" \
     out_dir "/home/dylan/Documents/SCZ/OUTPUTS" \
-    xnat_project "TEST_PROJ" \
+    xnat_project "BLSA" \
     xnat_subject "TEST_SUBJ" \
     xnat_session "TEST_SESS"
